ddfa0899ed5d46d9a45d02b650dda4c0535d497b
RamyaCGAithabattula <52093645+RamyaCGAithabattula@users.noreply.github.com>
2019-08-09 16:14:10 +0530

    Add files via upload

---
+++ wallet_back_end/src/main/java/com/cg/Application.java
@@ -0,0 +1,20 @@
+package com.cg;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Application {
+	 public static void main(String[] args) {
+	        SpringApplication.run(Application.class, args);
+	 }
+}
+
+
+
+
+
+
+
+
+/**The application port number 8085. 
+ *  http://localhost:8085/accounts with GET request to see the output, it will show all the initial accounts
+ */
